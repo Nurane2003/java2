d49c23635173caefbf5d13ff5266cb733d760770
Nurane2003 <nurane.sirinova.2003@gmail.com>
2024-03-04 00:48:31 +0400

    added new file

---
+++ index.js
@@ -0,0 +1,9 @@
+var name=prompt('Adiniz:')
+var age=prompt('Yasiniz:')
+var age2=prompt('Sizin yasiniz:')
+var ageInt=parseInt(age)
+
+if(age2>age){
+    alert('You are'+' '+(age2-age)+'years older than me')
+}
+  
