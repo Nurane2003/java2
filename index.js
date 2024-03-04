6680b151ba305adb63dc252d4d022d97837f5254
Nurane2003 <nurane.sirinova.2003@gmail.com>
2024-03-04 10:12:40 +0400

    added new file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name=prompt('Adiniz:')
+
 var age=prompt('Yasiniz:')
 var age2=prompt('Sizin yasiniz:')
 var ageInt=parseInt(age)
@@ -6,4 +6,6 @@ var ageInt=parseInt(age)
 if(age2>age){
     alert('You are'+' '+(age2-age)+'years older than me')
 }
-  
+  else{
+    alert("You are  yunger than me")
+  }
